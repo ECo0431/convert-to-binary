8f9716c49e7856c93ba8a62b892cb58059524c19
ChatCode <chatcode@MacBook-Pro-de-ChatCode.local>
2022-03-31 18:31:57 +0200

    commit

--- index.js
+++ index.js
@@ -93,195 +93,198 @@ function toBianary() {
   for (let i = 0; i < INPUT.value.length; i++) {
     switch (INPUT.value[i]) {
       case "a":
-        charBinary[i] = "01100001";
+        charBinary[i] = " 01100001";
         break;
       case "b":
-        charBinary[i] = "01100010";
+        charBinary[i] = " 01100010";
         break;
       case "c":
-        charBinary[i] = "01100011";
+        charBinary[i] = " 01100011";
         break;
       case "d":
-        charBinary[i] = "01100100";
+        charBinary[i] = " 01100100";
         break;
       case "e":
-        charBinary[i] = "01100101";
+        charBinary[i] = " 01100101";
         break;
       case "f":
-        charBinary[i] = "01100110";
+        charBinary[i] = " 01100110";
         break;
       case "g":
-        charBinary[i] = "01100111";
+        charBinary[i] = " 01100111";
         break;
       case "h":
-        charBinary[i] = "01101000";
+        charBinary[i] = " 01101000";
         break;
       case "i":
-        charBinary[i] = "01101001";
+        charBinary[i] = " 01101001";
         break;
       case "j":
-        charBinary[i] = "01101010";
+        charBinary[i] = " 01101010";
         break;
       case "k":
-        charBinary[i] = "01101011";
+        charBinary[i] = " 01101011";
         break;
       case "l":
-        charBinary[i] = "01101100";
+        charBinary[i] = " 01101100";
         break;
       case "m":
-        charBinary[i] = "01101101";
+        charBinary[i] = " 01101101";
         break;
       case "n":
-        charBinary[i] = "01101110";
+        charBinary[i] = " 01101110";
         break;
       case "o":
-        charBinary[i] = "01101111";
+        charBinary[i] = " 01101111";
         break;
       case "p":
-        charBinary[i] = "01110000";
+        charBinary[i] = " 01110000";
         break;
       case "q":
-        charBinary[i] = "01110001";
+        charBinary[i] = " 01110001";
         break;
       case "r":
-        charBinary[i] = "01110010";
+        charBinary[i] = " 01110010";
         break;
       case "s":
-        charBinary[i] = "01110011";
+        charBinary[i] = " 01110011";
         break;
       case "t":
-        charBinary[i] = "01110100";
+        charBinary[i] = " 01110100";
         break;
       case "u":
-        charBinary[i] = "01110101";
+        charBinary[i] = " 01110101";
         break;
       case "v":
-        charBinary[i] = "01110110";
+        charBinary[i] = " 01110110";
         break;
       case "w":
-        charBinary[i] = "01110111";
+        charBinary[i] = " 01110111";
         break;
       case "x":
-        charBinary[i] = "01111000";
+        charBinary[i] = " 01111000";
         break;
       case "y":
-        charBinary[i] = "01111001";
+        charBinary[i] = " 01111001";
         break;
       case "z":
-        charBinary[i] = "01111010";
+        charBinary[i] = " 01111010";
         break;
       case "A":
-        charBinary[i] = "01000001";
+        charBinary[i] = " 01000001";
         break;
       case "B":
-        charBinary[i] = "01000010";
+        charBinary[i] = " 01000010";
         break;
       case "C":
-        charBinary[i] = "01000011";
+        charBinary[i] = " 01000011";
         break;
       case "D":
-        charBinary[i] = "01000100";
+        charBinary[i] = " 01000100";
         break;
       case "E":
-        charBinary[i] = "01000101";
+        charBinary[i] = " 01000101";
         break;
       case "F":
-        charBinary[i] = "01000110";
+        charBinary[i] = " 01000110";
         break;
       case "G":
-        charBinary[i] = "01000111";
+        charBinary[i] = " 01000111";
         break;
       case "H":
-        charBinary[i] = "01001000";
+        charBinary[i] = " 01001000";
         break;
       case "I":
-        charBinary[i] = "01001001";
+        charBinary[i] = " 01001001";
         break;
       case "J":
-        charBinary[i] = "01001010";
+        charBinary[i] = " 01001010";
         break;
       case "K":
-        charBinary[i] = "01001011";
+        charBinary[i] = " 01001011";
         break;
       case "L":
-        charBinary[i] = "01001100";
+        charBinary[i] = " 01001100";
         break;
       case "M":
-        charBinary[i] = "01001101";
+        charBinary[i] = " 01001101";
         break;
       case "N":
-        charBinary[i] = "01001110";
+        charBinary[i] = " 01001110";
         break;
       case "O":
-        charBinary[i] = "01001111";
+        charBinary[i] = " 01001111";
         break;
       case "P":
-        charBinary[i] = "01010000";
+        charBinary[i] = " 01010000";
         break;
       case "Q":
-        charBinary[i] = "01010001";
+        charBinary[i] = " 01010001";
         break;
       case "R":
-        charBinary[i] = "01010010";
+        charBinary[i] = " 01010010";
         break;
       case "S":
-        charBinary[i] = "01010011";
+        charBinary[i] = " 01010011";
         break;
       case "T":
-        charBinary[i] = "01010100";
+        charBinary[i] = " 01010100";
         break;
       case "U":
-        charBinary[i] = "01010101";
+        charBinary[i] = " 01010101";
         break;
       case "V":
-        charBinary[i] = "01010110";
+        charBinary[i] = " 01010110";
         break;
       case "W":
-        charBinary[i] = "01010111";
+        charBinary[i] = " 01010111";
         break;
       case "X":
-        charBinary[i] = "01011000";
+        charBinary[i] = " 01011000";
         break;
       case "Y":
-        charBinary[i] = "01011001";
+        charBinary[i] = " 01011001";
         break;
       case "Z":
-        charBinary[i] = "01011010";
+        charBinary[i] = " 01011010";
         break;
       case " ":
-        charBinary[i] = "00100000";
+        charBinary[i] = " 00100000";
         break;
       case "0":
         charBinary[i] = "";
         break;
       case "1":
-        charBinary[i] = "00110001";
+        charBinary[i] = " 00110001";
         break;
       case "2":
-        charBinary[i] = "00110010";
+        charBinary[i] = " 00110010";
         break;
       case "3":
-        charBinary[i] = "00110011";
+        charBinary[i] = " 00110011";
         break;
       case "4":
-        charBinary[i] = "00110100";
+        charBinary[i] = " 00110100";
         break;
       case "5":
-        charBinary[i] = "00110101";
+        charBinary[i] = " 00110101";
         break;
       case "6":
-        charBinary[i] = "00110110";
+        charBinary[i] = " 00110110";
         break;
       case "7":
-        charBinary[i] = "00110111";
+        charBinary[i] = " 00110111";
         break;
       case "8":
-        charBinary[i] = "00111000";
+        charBinary[i] = " 00111000";
         break;
       case "9":
-        charBinary[i] = "00111001";
+        charBinary[i] = " 00111001";
         break;
-
+      case "é":
+        charBinary[i] =
+          " 0010011001100101011000010110001101110101011101000110010100111011";
+        break;
       default:
         console.log("valeur non listé");
     }
